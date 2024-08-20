98c918cb29c88ab2b854975d21eb38579253a8dd
Gabriel Alfaro <gabriel.alfaro.cruz@gmail.com>
2024-08-20 13:40:06 -0600

    Modificaciones por dotenv

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Permite que un cliente se conecte a un servidor para el intercambio de datos
-require('dotenv').config()
+require('dotenv').config({path:'variables.env'})
 
 
 // Verificar variables de entorno
@@ -32,8 +32,23 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Definir un dominio para recibir las peticiones
+
+const whitelist = [process.env.FRONTEND_URL, process.env.BACKEND_URL]; // Agrega el dominio correcto
+const corsOptions = {
+    origin: (origin, callback) => {
+        //Revisar si la peticion viene de un servidor que estamos permitiendo en la lista blanca
+        const existe = whitelist.some(dominio => dominio === origin);
+        if (existe || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}
+
 //Habilitar CORS
-app.use(cors());
+app.use(cors(corsOptions));
 
 //Rutas de la app
 app.use('/', routes());
@@ -42,4 +57,5 @@ app.use('/', routes());
 app.use(express.static('uploads'));
 
 //Asignamos el puerto al servidor
-app.listen(5000);
+// app.listen(5000);
+app.listen(process.env.PORT || 5000, process.env.HOST || '0.0.0.0',() => console.log('Servidor iniciado...'));
